29d2624603c7b5f121936d9f6f8b2bbacaad8e7e
Sahil Pansare <sahilpansare1717@gmail.com>
2026-08-22 22:09:33 +0530

    Palindrome check of a word

--- Palindrome.java
+++ Palindrome.java
@@ -2,24 +2,19 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        int number = sc.nextInt();
+        System.out.print("Please enter a word to check if it's a palindrome : ");
+        String word = sc.nextLine();
 
-        int originalNumber = number;
-        int reverse = 0;
+        StringBuilder reverse = new StringBuilder(word);
+        reverse.reverse();
 
-        while (number != 0) {
-            int digit = number % 10;
-            reverse = reverse * 10 + digit;
-            number = number / 10;
-        }
-
-        if (originalNumber == reverse) {
-            System.out.println(originalNumber + " is a palindrome number.");
+        if (word.equalsIgnoreCase(reverse.toString())) {
+            System.out.println(word + " is a palindrome.");
         } else {
-            System.out.println(originalNumber + " is not a palindrome number.");
+            System.out.println(word + " is not a palindrome.");
         }
 
         sc.close();
